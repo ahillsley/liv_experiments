8c6162c3b19fee90c56b893cbbe5b78577fbcbad
Alex Hillsley <avhillsley@utexas.edu>
2024-02-21 12:27:23 -0500

    remove paths from veins that are not in cell_paths

--- liv_experiments/create_cell_datasets.py
+++ liv_experiments/create_cell_datasets.py
@@ -22,7 +22,14 @@ path = "/groups/feliciano/felicianolab/For_Alex_and_Mark/After_median_filter"
 cell_paths = glob.glob(f'{path}/*/*/*/*/average_properties_per_cell.csv', recursive=True)
 # create cell level dataset
 cell_paths = glob.glob(f'{path}/*/*/*/*/average_properties_per_cell.csv', recursive=True)
+portal_paths = glob.glob(f'{path}/*/*/*/*/portal_dist.npy', recursive=True)
+central_paths = glob.glob(f'{path}/*/*/*/*/central_dist.npy', recursive=True)
 
+# trim protal and central paths to be same element wise as cell_paths
+del portal_paths[7]
+del portal_paths[26]
+del central_paths[7]
+del central_paths[26]
 #%%
 kept_props = [
     'mito_density',
@@ -46,12 +53,9 @@ kept_props = [
 
 asinus = 0
 multi_asinus_cell_dataset = pd.DataFrame()
-for asinus in range(len(peroxi_paths)):
+for asinus in range(len(cell_paths)):
     # read csvs
     print(f'processing num {asinus}')
-    if 'Liv2' in cell_paths[asinus]:
-        print(f'skipping path: {cell_paths[asinus]}')
-        continue
 
     cell_props = pd.read_csv(cell_paths[asinus])
 
@@ -61,22 +65,20 @@ for asinus in range(len(peroxi_paths)):
     scaled_x_cuts, crop_shape = vein_cutoffs(portal_path, central_path)
 
     c_props = cell_props[kept_props]
-    print(c_props.mean())
     c_props = (c_props - c_props.mean()) / c_props.std()
     
-
     standardized_dataset = c_props
-    standardized_dataset['centroid-0'] = cell_props['centroid-0'] / crop_shape[0]
-    standardized_dataset['centroid-1'] = cell_props['centroid-1'] / crop_shape[1]
+    standardized_dataset['centroid-0'] = cell_props['centroid-0'] / 22.187
+    standardized_dataset['centroid-1'] = cell_props['centroid-1'] / 22.187
     standardized_dataset['asinus'] = asinus
     standardized_dataset['ascini_position'] = cell_props['ascini_position']
 
-    cut_1 = standardized_dataset['centroid-1'] > scaled_x_cuts[0]
-    cut_2 = standardized_dataset['centroid-1'] < scaled_x_cuts[1]
+    cut_1 = standardized_dataset['centroid-1'] > scaled_x_cuts[0] * crop_shape[1] / 22.187
+    cut_2 = standardized_dataset['centroid-1'] < scaled_x_cuts[1] * crop_shape[1] / 22.187
     cutoff = pd.concat([cut_1, cut_2], axis=1)
     within_cutoff = cutoff.all(axis=1)
     standardized_dataset['cutoff'] = within_cutoff * 1
     multi_asinus_cell_dataset = pd.concat([multi_asinus_cell_dataset, standardized_dataset], axis=0)
 # %%
-multi_asinus_cell_dataset.to_csv(f'cell_dataset_12_13_23.csv')
+multi_asinus_cell_dataset.to_csv(f'../datasets/cell_dataset_02_21_24.csv')
 # %%
